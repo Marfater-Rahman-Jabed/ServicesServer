0239364170bfd9e31ee91023f608b7956219b869
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2023-12-13 23:08:27 +0600

    added excel operation

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     try {
         const databaseCollection = client.db('MyService').collection('databaseCollection')
         const UserCollection = client.db('MyService').collection('UserCollection')
+        const excelCollection = client.db('MyService').collection('excelCollection')
 
         app.get('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -44,6 +45,24 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
+        app.get('/excelfind/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {
+                clientEmail: email
+            }
+            const cursor = excelCollection.find(query).sort({ _id: -1 })
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/excelDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const cursor = excelCollection.find(query).sort({ _id: -1 })
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
         app.post('/addUser', async (req, res) => {
             const query = req.body;
@@ -53,6 +72,11 @@ async function run() {
         app.post('/uploadDatabase', async (req, res) => {
             const query = req.body;
             const result = await databaseCollection.insertOne(query)
+            res.send(result)
+        })
+        app.post('/uploadExcel', async (req, res) => {
+            const query = req.body;
+            const result = await excelCollection.insertOne(query)
             res.send(result)
         })
 
@@ -68,7 +92,7 @@ async function run() {
             const updateDoc = {
                 $set: {
                     colNo: bodyData.colNo,
-                    colName: bodyData.data
+                    colName: bodyData.filteredArray
                 }
             }
             const result = await UserCollection.updateOne(filter, updateDoc, options)
@@ -91,6 +115,23 @@ async function run() {
             const result = await databaseCollection.updateOne(filter, updateDoc, options)
             res.send(result);
         })
+        app.put('/updateExcelSheetName/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const bodyData = req.body
+            // const query = {
+            //     email
+            // }
+            // const filter = { email: `${query}` };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    SheetName: bodyData.data.SheetName
+                }
+            }
+            const result = await excelCollection.updateOne(filter, updateDoc, options)
+            res.send(result);
+        })
 
         app.delete('/deleteDatabase/:id', async (req, res) => {
             const id = req.params.id;
@@ -98,6 +139,15 @@ async function run() {
                 _id: new ObjectId(id)
             }
             const result = await databaseCollection.deleteOne(query)
+            res.send(result);
+
+        })
+        app.delete('/deleteExcelSheet/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await excelCollection.deleteOne(query)
             res.send(result);
 
         })
